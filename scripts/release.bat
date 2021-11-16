89084fab7081b02fe1092386a90d3c8174d05665
Thibault H <Aelto@users.noreply.github.com>
2021-11-16 20:25:32 +0100

    Initial commit

---
+++ scripts/release.bat
@@ -0,0 +1,12 @@
+call variables.cmd
+
+rmdir "%modpath%\release" /s /q
+mkdir "%modpath%\release"
+
+mkdir "%modpath%\release\mods\%modname%\content\scripts\"
+rmdir "%modpath%\release\mods\%modName%\content\" /s /q
+XCOPY "%modpath%\src" "%modpath%\release\mods\%modName%\content\scripts\" /e /s /y
+XCOPY "%modpath%\strings" "%modpath%\release\mods\%modName%\content\" /e /s /y
+
+mkdir "%modpath%\release\bin\config\r4game\user_config_matrix\pc\"
+copy "%modpath%\mod-menu.xml" "%modpath%\release\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
